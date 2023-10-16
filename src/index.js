11e87733f77796aa74dc46f3acf00fb525683030
weemizo <mrceleius@gmail.com>
2023-10-16 21:25:16 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,38 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const Image = ({ src }) => {
-    return (
-        <div>
-            <img src={src} alt="" /> 
-        </div>
-    )
-}
-
-const Title = ({ title }) => {
-    return (
-        <div>
-            {title}
-        </div>
-    )
-}
-
-const Author = ({ author }) => {
-    return (
-        <div>
-            <h6>
-                {author}
-            </h6>
-        </div>
-    )
-}
-
-const Book = ({ src, title, author}) => {
+const Book = ({src, title, author}) => {
     return (
         <div className='book'>
-            <Image src={src} />
-            <Title  title={title} />
-            <Author author={author} />
+            <img src={src} alt="" /> 
+            <h2>{title}</h2>
+            <h4>{author.toUpperCase()}</h4>
         </div>
     )
 }
@@ -53,8 +27,6 @@ function BestSellers() {
 }
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BestSellers />
-    </React.StrictMode>,
+        <BestSellers />,
     document.getElementById('root')
 )
